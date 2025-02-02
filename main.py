ba7423b07c441657093ad9ad5cf84d85870719c8
Adi Kadimetla <kadimetla@gmail.com>
2025-02-02 15:17:12 -0500

    initial commit

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-# This is a sample Python script.
 import csv
 from datetime import datetime
 
 from expense_tracker import ExpenseTracker
-
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 def console_menu():
     print("**********************  Welcome to Expense Tracker! ****************************")
